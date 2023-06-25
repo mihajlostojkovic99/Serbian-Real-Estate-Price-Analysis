a71c65524408960f92c5dcd9f7825e36b4d2a124
Mihajlo Stojkovic <mihajlostojkovic99@gmail.com>
2023-06-25 14:53:33 +0200

    dropped legacy tables

---
+++ drizzle/0011_white_argent.sql
@@ -0,0 +1,4 @@
+DROP TABLE "apartments_for_rent";--> statement-breakpoint
+DROP TABLE "apartments_for_sale";--> statement-breakpoint
+DROP TABLE "houses_for_rent";--> statement-breakpoint
+DROP TABLE "houses_for_sale";
